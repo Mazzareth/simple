0cdd13d8341614899bd305ecef6baa794ed747ef
Mazze <mercysquadrant@gmail.com>
2026-03-07 17:02:29 -0800

    Initial commit

---
+++ src/client/java/app/masterwork/simple/client/SimpleClient.java
@@ -0,0 +1,10 @@
+package app.masterwork.simple.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/app/masterwork/simple/Simple.java
@@ -0,0 +1,10 @@
+package app.masterwork.simple;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Simple implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
